56fff96845e02b2c2145dabb2755b829f665c245
Thor Anderson <ta.anderson@gmail.com>
2018-10-01 15:33:56 -0600

    Testing new v-list code

--- src/components/Servers.vue
+++ src/components/Servers.vue
@@ -14,6 +14,28 @@
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
+
+<v-list>
+          <v-list-tile
+            v-for="item in items"
+            :key="item.title"
+            avatar
+            @click=""
+          >
+            <v-list-tile-action>
+              <v-icon v-if="item.icon" color="pink">star</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title v-text="item.title"></v-list-tile-title>
+            </v-list-tile-content>
+
+            <v-list-tile-avatar>
+              <img :src="item.avatar">
+            </v-list-tile-avatar>
+          </v-list-tile>
+        </v-list>
+
 </v-card>
 </v-flex>
 </template>
